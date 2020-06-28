cf9bc6789273d702df2810bd2ddffb0529f9e44a
Mike Rock <mr392@njit.edu>
2020-06-27 23:11:08 -0400

    Task: set up environment

--- src/test.py
+++ src/test.py
@@ -1,5 +1,47 @@
+import random
 
-class Ran_Gen:
+class RanGen:
   def __init__(self):
-    self.name = name
-    self.age = age
+     pass
+
+  @staticmethod
+  def random_no_seed():
+       random_num = random.uniform(1, 20)
+       return random_num
+
+  @staticmethod
+  def random_with_seed():
+       random.seed(6)
+       random_num = random.uniform(1.1,20.5)
+       return random_num
+
+
+
+
+
+
+
+
+
+
+
+generate_number = RanGen()
+
+msg = generate_number.random_with_seed()
+print(msg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
