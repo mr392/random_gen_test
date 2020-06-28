716c71f817d974599761e5ea8f8c66b88e47915d
Mike Rock <mr392@njit.edu>
2020-06-28 17:40:04 -0400

    Task: First Test added

--- src/RandomGeneratorsTest.py
+++ src/RandomGeneratorsTest.py
@@ -3,11 +3,16 @@ import RandomGenerators
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
+        """List evaluates to False if empty"""
         test_list = RandomGenerators.list_generator(1)
 
         self.assertTrue(test_list)
 
 
+    def test_something(self):
+        test_list = RandomGenerators.list_generator(1)
+
+        self.assertFalse(test_list)
 
 
 if __name__ == '__main__':
