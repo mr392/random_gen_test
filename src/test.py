8227f2e7a8f117c17533ab334c0ac23410af1431
Mike Rock <mr392@njit.edu>
2020-06-28 17:01:26 -0400

    Task: set up environment

--- src/test.py
+++ src/test.py
@@ -1,21 +1,19 @@
 import random
 
-class RanGen:
-  def __init__(self):
-     pass
 
-  @staticmethod
+
+
   def random_no_seed():
        random_num = random.uniform(1, 20)
        return random_num
 
-  @staticmethod
+
   def random_with_seed(seed):
        random.seed(6)
        random_num = random.uniform(1.1,20.5)
        return random_num
 
-  @staticmethod
+
   def list_generator(seed):
 
       random_list = []
@@ -28,25 +26,25 @@ class RanGen:
           random_list.append(random_num)
       return random_list
 
-  @staticmethod
+
   #attach CSV reader?
   def random_item():
       # used above function for testing
-      test_list = RanGen.list_generator(12)
+      test_list = list_generator(12)
 
       choice = random.choice(test_list)
       return choice
 
-  @staticmethod
+
   def random_seed_choice():
       # used above function for testing
-      test_list = RanGen.list_generator(12)
+      test_list = list_generator(12)
 
       random.seed(6)
       choice = random.choice(test_list)
       return choice
 
-  @staticmethod
+
   #for seed and not seed
   def n_items_no_seed(choices, seed = None):
 
@@ -55,16 +53,16 @@ class RanGen:
 
       number_of_choices = choices
 
-      test_list = RanGen.list_generator(seed)
+      test_list = list_generator(seed)
 
       sample = random.choices(test_list, k=number_of_choices)
       return sample
 
 
-generate_number = RanGen()
+
 
 # change to what func you want to test
-msg = generate_number.n_items_no_seed(10, 100)
+msg = n_items_no_seed(10, 100)
 
 print(msg)
 
